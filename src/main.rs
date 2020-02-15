1bc134d247f382a4bd6be9b1b73bbf2eae02e5ab
Ken Sato <ksato9700@gmail.com>
2020-02-15 23:54:10 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+const API_ROOT: &str = "/api/v1";
+const HOSTPORT: &str = "0.0.0.0:3000";
+
+async fn get_books() -> impl Responder {
+  "all books"
+}
+
+async fn get_book_by_id(book_id: web::Path<u32>) -> impl Responder {
+  format!("Book id:{}", book_id)
+}
+
+async fn get_book_card(_book_id: web::Path<u32>) -> impl Responder {
+  HttpResponse::NotImplemented()
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+  // std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+  env_logger::init();
+
+  HttpServer::new(|| {
+    App::new().wrap(Logger::new("%a %r %s %b")).service(
+      web::scope(API_ROOT)
+        .service(web::resource("/books").to(get_books))
+        .service(web::resource("/books/{id}").to(get_book_by_id))
+        .service(web::resource("/books/{id}/card").to(get_book_card)),
+    )
+  })
+  .bind(HOSTPORT)?
+  .run()
+  .await
+}
